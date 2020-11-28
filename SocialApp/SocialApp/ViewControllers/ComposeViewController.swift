875ff2c6cfc6a499f22d31acc9cbff246b9d0a94
zazzaze <egor.anikeev.00@gmail.com>
2020-11-29 02:05:16 +0300

    Сделан макет

---
+++ SocialApp/SocialApp/ViewControllers/ComposeViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ComposeViewController.swift
+//  SocialApp
+//
+//  Created by Егор on 29.11.2020.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController {
+
+    @IBOutlet weak var tweetContent: UITextView!
+    @IBOutlet weak var postButton: UIButton!
+    @IBOutlet weak var postActivity: UIActivityIndicatorView!
+    @IBAction func dismissView(_ sender: Any) {
+    }
+    @IBAction func postToTwitter(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SocialApp/SocialApp/ViewControllers/TweetViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TweetViewController.swift
+//  SocialApp
+//
+//  Created by Егор on 29.11.2020.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet weak var tweetAuthorAvatar: UIImageView!
+    @IBOutlet weak var tweetAuthorName: UILabel!
+    @IBOutlet weak var tweetText: UITextView!
+    @IBAction func dismissView(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
