29bc261ff967f3809351abbcb11d6d1cdfad3e47
zazzaze <egor.anikeev.00@gmail.com>
2021-03-05 23:11:06 +0300

    Done

---
+++ TasksTracker/Pods/Target Support Files/Pods-CoreDataFramework/Pods-CoreDataFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreDataFramework : NSObject
+@end
+@implementation PodsDummy_Pods_CoreDataFramework
+@end
---
+++ TasksTracker/Pods/Target Support Files/Pods-TaskWidgetExtension/Pods-TaskWidgetExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TaskWidgetExtension : NSObject
+@end
+@implementation PodsDummy_Pods_TaskWidgetExtension
+@end
